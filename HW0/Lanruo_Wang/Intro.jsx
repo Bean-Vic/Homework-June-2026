af8be72e32374df773a91a5e7f84fd3ad826aa79
lab3_mini <lr.www@hotmail.com>
2026-06-02 23:37:46 -0500

    HW0 Lanruo Wang

---
+++ HW0/Lanruo_Wang/Intro.jsx
@@ -0,0 +1,11 @@
+function Intro() {
+  return (
+    <div>
+      <h1>HW0 React Practice</h1>
+      <p>This is my first React app created with Create React App.</p>
+      <p>Name: Lanruo Wang</p>
+    </div>
+  );
+}
+
+export default Intro;
