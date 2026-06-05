bd9d1c88e1c936ab2f5eda72e1b1a3f335b56274
maozaida22 <jesseshui66@gmail.com>
2026-06-05 15:03:19 -0700

    Add files via upload

---
+++ HW2/Jesse/2026-06-05-LC-268.js
@@ -0,0 +1,11 @@
+
+var missingNumber = function(nums) {
+    let expectedSum = nums.length * (nums.length + 1) / 2;
+    let actualSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        actualSum += nums[i];
+    }
+
+    return expectedSum - actualSum;
+};
