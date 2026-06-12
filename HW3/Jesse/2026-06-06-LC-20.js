503dfe4af211fe7fa9a65a63e47308ef24c5538b
maozaida22 <jesseshui66@gmail.com>
2026-06-12 15:46:09 -0700

    Add files via upload

---
+++ HW3/Jesse/2026-06-06-LC-20.js
@@ -0,0 +1,18 @@
+var isValid = function (s) {
+  const stack = [];
+  const pairs = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+  };
+
+  for (const char of s) {
+    if (char === '(' || char === '[' || char === '{') {
+      stack.push(char);
+    } else if (stack.pop() !== pairs[char]) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};
