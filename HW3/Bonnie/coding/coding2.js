cebb55611c2b78d0c2bb64868dc7f9e3993e9375
BonnieWZY <yizinext@gmail.com>
2026-06-21 01:58:54 -0400

    coding 2

--- HW3/Bonnie/coding/coding2.js
+++ HW3/Bonnie/coding/coding2.js
@@ -4,3 +4,15 @@
 // Given the string, implement a function to replace the - character with space,
 // and remove extra space in the string and convert the string to all lowercase
 
+const str =
+" Perhaps The Easiest-to-understand Case For Reduce Is To Return The Sum Of All The Elements In An Array";
+
+function cleanString(str) {
+    return str
+        .toLowerCase()
+        .replaceAll("-", " ")
+        .replace(/\s+/g, " ")
+        .trim();
+}
+
+console.log(cleanString(str));
